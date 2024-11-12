4f9c7a5bd4ae3dfcd0724453cd039e1ce7a91756
NafSyed <nafeezsyed.2003@gmail.com>
2024-11-12 09:25:37 +0530

    Initial Commit

---
+++ Swipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swipe
+//
+//  Created by Nafeez Ahmed on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by Nafeez Ahmed on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
